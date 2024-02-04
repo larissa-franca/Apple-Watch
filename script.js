d4197bd77126e13b26846228ae082729c382f95d
larissaRF <larissa_lrf@hotmail.com>
2024-02-04 18:48:31 -0300

    js

---
+++ script.js
@@ -0,0 +1,24 @@
+const verdeCipreste = {
+    nome: 'Verde-Cipreste',
+    pasta: 'imagens-verde-cipreste'
+}
+
+const azulInverno = {
+    nome: 'Azul-Inverno',
+    pasta: 'imagens-azul-inverno'
+}
+
+const meiaNoite = {
+    nome: 'Meia-Noite',
+    pasta: 'imagens-meia-noite'
+}
+
+const estelar = {
+    nome: 'Estelar',
+    pasta: 'imagens-estelar'
+}
+
+const rosaClaro = {
+    nome: 'Rosa-Claro',
+    pasta: 'imagens-rosa-claro'
+}
